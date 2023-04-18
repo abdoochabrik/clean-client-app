bd5dec161041fb4ed41264bfc811f7cf61e7fead
chabrik abdellatif <abdellatif.chabrik.fycomputing@gmail.com>
2023-04-18 21:18:06 +0000

    refactor(login): :zap: first MVVM skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponentViewModel } from './components/login/login.component.viewModel';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentViewModel
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/login/login.component.viewModel.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.vieW.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponentViewModel implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
