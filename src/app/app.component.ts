4bf7672ed17f162e32755f68f2df139a94e5424e
chabrik abdellatif <abdellatif.chabrik.fycomputing@gmail.com>
2023-05-18 20:10:16 +0100

    build app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+export class TodoNotFoundError extends Error {}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Library';
-}
+
+  /**
+   * @throws TodoNotFoundErro
+   */
+  public hello():void{
+
+  }
+ }
